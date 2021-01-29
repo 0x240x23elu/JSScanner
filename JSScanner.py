6ff6c110199f969b0ec7e71b17d99ec2deb2ad25
0x240x23elu <52744492+0x240x23elu@users.noreply.github.com>
2021-01-29 13:07:12 +0530

    new Update

--- JSScanner.py
+++ JSScanner.py
@@ -1,6 +1,8 @@
-
-import requests 
-import requests as r 
+#import faster_than_requests as requests
+import concurrent.futures
+import requests
+from concurrent import futures
+#import requests as r 
 import re 
 import urllib3 
 import sys 
@@ -38,37 +40,48 @@ print("╔═══════════════════════�
       "║                                                                ║\n"
       "╚════════════════════════════════════════════════════════════════╝")
 
-path = input("Please Enter Js File: ") 
-#path2 = input("Patten file ")
+path = input("Please Enter Any File: ") 
+reg = input("Path Of Regex/Patten File: ")
 list=[] 
 file1 = open(path, 'r')
 Lines = file1.readlines() 
 count = 0
 # Strips the newline character
 for line in Lines: 
-    ip = line.strip() 
+    ip = line.strip()
+    print(colored.fg("white"), ip)
     try:
-        r = requests.get(ip, allow_redirects=True, verify=False)
-        print(colored.fg("white"), ip)
-        file2 = open('regex.txt', 'r')
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [
+                executor.submit(
+                    lambda: requests.get(ip))
+            for _ in range(1)
+        ]
+            
+
+        results = [
+            f.result().text
+            for f in futures
+        ]
+
+        
+        
+        file2 = open(reg, 'r')
         Lines2 = file2.readlines()
         for line2 in Lines2: 
             regex = line2.strip()
-            #print(regex)
-
-           
-            matches = re.finditer(regex, r.text, re.MULTILINE)
-            
-
+        #print(regex)
+            matches = re.finditer(regex, str(results), re.MULTILINE)
             for matchNum, match in enumerate(matches, start=1):
-        
+    
                 print (colored.fg("green") ,"Regex: ",regex)
                 print(colored.fg("red") , "Match {matchNum} was found at: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()), '\n')
-                f = open('output.txt', 'a')
+                f = open('out.txt.txt', 'a')
                 L = [ip, '\n', "Regex: ", regex, '\n', "Match {matchNum} was found at : {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()),'\n']
                 f.writelines(L)
                 f.close()
-            
+           
     except requests.exceptions.RequestException as e:
         # A serious problem happened, like an SSLError or InvalidURL
         print("Error: {}".format(e))    
+
