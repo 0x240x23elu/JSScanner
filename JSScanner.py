33e2a25a20d4938f98c6e4f1e7b876e4ab08dacf
Hugo <30708062+sukenn@users.noreply.github.com>
2021-08-03 00:08:20 -0300

    Update JSScanner.py

--- JSScanner.py
+++ JSScanner.py
@@ -11,35 +11,7 @@ import colored
 from colored import stylize
 import argparse
 urllib3.disable_warnings()
-print(colored.fg("red"), 
-     "╔════════════════════════════════════════════════════════════════╗\n"
-      "║                    Developed By 0x240x23elu                      ║\n"
-      "║                                                                ║\n"
-      "╚════════════════════════════════════════════════════════════════╝")
-print("╔════════════════════════════════════════════════════════════════╗\n"
-      "║                                                                ║\n"
-      "║                           WARNING                              ║\n"
-      "║                                                                ║\n"
-      "║      I highly recommend using this tool by using Kali Linux OS ║\n"
-      "║                                                                ║\n"
-      "║      By using this tool it means you agree with terms,         ║\n"
-      "║      conditions, and risks                                     ║\n"
-      "║                                                                ║\n"
-      "║      By using this tool you agree that                         ║\n"
-      "║      1. use for legitimate security testing                    ║\n"
-      "║      2. not for crime                                          ║\n"
-      "║      3. the use of this tool solely for                        ║\n"
-      "║         educational reasons only                               ║\n"
-      "║                                                                ║\n"
-      "║      By using this tool you agree that                         ║\n"
-      "║      1. You are willing to be charged with criminal or state   ║\n"
-      "║         law applicable by law enforcement officers             ║\n"
-      "║         and government when abused                             ║\n"
-      "║      2. the risk is borne by yourself                          ║\n"
-      "║                                                                ║\n"
-      "║         Thank you and happy pentest                            ║\n"
-      "║                                                                ║\n"
-      "╚════════════════════════════════════════════════════════════════╝")
+print(colored.fg("red"), "JSScan developed by @0x240x23elu adjusted by @sukenn")
 
 parser = argparse.ArgumentParser(usage="python3 JSScanner.py --urls jsfiles.txt --regex regex.txt --output output.txt")
 optional = parser._action_groups.pop()
@@ -58,6 +30,8 @@ if path is None:
     path = input("Please Enter Any File: ") 
 if reg is None:
     reg = input("Path Of Regex/Patten File: ")
+if output is None:
+    output = input("Path To Save Output File: ")
 
 list=[] 
 file1 = open(path, 'r')
@@ -93,10 +67,7 @@ for line in Lines:
                 print (colored.fg("green") ,"Regex: ",regex)
                 print(colored.fg("green"), ip)
                 print(colored.fg("red") , "Match {matchNum} was found at: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()), '\n')
-                if output is None:
-                    f = open('out.txt.txt', 'a')
-                else:
-                    f = open(output, 'a')
+                f = open(output, 'a')
                 L = [ip, '\n', "Regex: ", regex, '\n', "Match {matchNum} was found at : {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()),'\n']
                 f.writelines(L)
                 f.close()
